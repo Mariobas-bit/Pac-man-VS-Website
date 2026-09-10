7e855108ed2d54c7c98da1a19afcd8e679831de6
Mariobas-bit <Mariobas1325@gmail.com>
2026-09-10 11:37:24 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -19,7 +19,6 @@ class MainMenuScene extends Phaser.Scene {
         this.renderMenuText();
         
         if (this.menuMode === "Wait") {
-            })
             socket.on('server_on', (data) => {
                this.menuMode = "Select"; 
             });
